3c9ec7d0c1ff142326c16f4e63838487a7a9f920
hyeoz <jenny970203@gmail.com>
2024-01-06 15:18:48 +0900

    Initialize project using Create React App

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,31 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+>>>>>>> 3e6342d (Initialize project using Create React App)
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+<<<<<<< HEAD
+=======
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> 3e6342d (Initialize project using Create React App)
