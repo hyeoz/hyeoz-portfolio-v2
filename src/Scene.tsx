d458dfad5ac09d7cb0131f4919aa779b4b5242e4
hyeoz <jenny970203@gmail.com>
2024-01-25 09:12:40 +0900

    add: 모델 렌더링 준비 중

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,12 +1,14 @@
 import { Scroll, ScrollControls } from "@react-three/drei";
 import Html from "./components/Html";
 import Particles from "./components/Particles";
+import Airplane from "./components/Airplane";
 
 function Scene() {
   return (
     <ScrollControls pages={5}>
       <Scroll>
         <Particles size={7000} />
+        <Airplane />
       </Scroll>
       <Scroll html>
         <Html />
