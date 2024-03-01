850a42e774c9db09e463a709388ee05300ddf089
hyeoz <jenny970203@gmail.com>
2024-03-01 19:29:21 +0900

    chore: change boilerplate

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { OrbitControls, Scroll, ScrollControls } from "@react-three/drei";
 import Html from "./components/Html";
 import { Suspense } from "react";
@@ -9,10 +10,23 @@ import Ball from "./components/Ball";
 import { Scroll, ScrollControls } from "@react-three/drei";
 import Html from "./components/Html";
 >>>>>>> 840b172 (add: r3f 기본 세팅)
+=======
+import { Scroll, ScrollControls } from "@react-three/drei";
+import Html from "./components/Html";
+=======
+import { OrbitControls, Scroll, ScrollControls } from "@react-three/drei";
+import Html from "./components/Html";
+import { Suspense } from "react";
+import Particles from "./components/Particles";
+import Laptop from "./components/Laptop";
+import Ball from "./components/Ball";
+>>>>>>> c6989d8 (chore: change boilerplate)
+>>>>>>> 6ca9d52 (chore: change boilerplate)
 
 function Scene() {
   return (
     <ScrollControls pages={5}>
+<<<<<<< HEAD
 <<<<<<< HEAD
       <Scroll>
         <Laptop />
@@ -22,6 +36,16 @@ function Scene() {
 =======
       <Scroll></Scroll>
 >>>>>>> 840b172 (add: r3f 기본 세팅)
+=======
+      <Scroll></Scroll>
+=======
+      <Scroll>
+        <Laptop />
+        {/* <Ball /> */}
+        <Particles size={7000} />
+      </Scroll>
+>>>>>>> c6989d8 (chore: change boilerplate)
+>>>>>>> 6ca9d52 (chore: change boilerplate)
       <Scroll html>
         <Html />
       </Scroll>
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 =======
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 >>>>>>> 389a7d2 (init: react + vite + ts 기본 세팅)
+=======
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+>>>>>>> 6ca9d52 (chore: change boilerplate)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 6ca9d52 (chore: change boilerplate)
   server: {
     port: 3000,
   },
 });
+<<<<<<< HEAD
 =======
 })
 >>>>>>> 389a7d2 (init: react + vite + ts 기본 세팅)
+=======
+>>>>>>> 6ca9d52 (chore: change boilerplate)
