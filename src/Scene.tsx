d505eb21b4b4dd6f150a4a22ee1f1da28b11c01c
hyeoz <jenny970203@gmail.com>
2024-03-03 22:22:54 +0900

    add: scroll manager 추가

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,17 +1,21 @@
 import { Scroll, ScrollControls } from '@react-three/drei';
+import { useState } from 'react';
 
 import Html from './components/Html';
-import Particles from './components/Particles';
+// import Particles from './components/Particles';
 import Laptop from './components/Laptop';
-import Ball from './components/Ball';
+// import Ball from './components/Ball';
+import ScrollManagre from './components/ScrollManager';
 
 function Scene() {
+  const [section, setSection] = useState(0);
   return (
-    <ScrollControls pages={5}>
+    <ScrollControls pages={4}>
+      <ScrollManagre section={section} setSection={setSection} />
       <Scroll>
         <Laptop />
-        <Ball />
-        <Particles size={7000} />
+        {/* <Ball /> */}
+        {/* <Particles size={7000} /> */}
       </Scroll>
       <Scroll html>
         <Html />
