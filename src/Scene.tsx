6d9d99fa57184a46125a17765ba3a927d3bb1f35
hyeoz <jenny970203@gmail.com>
2024-03-06 23:45:57 +0900

    add: 비행기 모델 추가, page section 작업 중

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -6,13 +6,19 @@ import Html from './components/Html';
 import Laptop from './components/Laptop';
 // import Ball from './components/Ball';
 import ScrollManagre from './components/ScrollManager';
+import Airplane from './components/Airplane';
 
 function Scene() {
   const [section, setSection] = useState(0);
+
   return (
-    <ScrollControls pages={4}>
+    <ScrollControls pages={4} damping={0.1}>
       <ScrollManagre section={section} setSection={setSection} />
       <Scroll>
+        {/* <Airplane position={[0, 0, 0]} /> */}
+        <Laptop />
+        <Laptop />
+        <Laptop />
         <Laptop />
         {/* <Ball /> */}
         {/* <Particles size={7000} /> */}
--- src/components/ScrollManager.tsx
+++ src/components/ScrollManager.tsx
@@ -1,4 +1,4 @@
-import { useScroll } from '@react-three/drei';
+import { ScrollControlsState, useScroll } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
 import gsap from 'gsap';
@@ -10,9 +10,13 @@ export default function ScrollManagre({
   section: number;
   setSection: Dispatch<SetStateAction<number>>;
 }) {
-  const scroll = useScroll();
+  const scroll: ScrollControlsState & { scroll?: { current: number } } =
+    useScroll();
   const lastScroll = useRef(0);
   const isAnimating = useRef(false);
+
+  scroll.fill.style['position'] = 'absolute';
+  scroll.fill.style['top'] = '0';
 
   useEffect(() => {
     gsap.to(scroll.el, {
@@ -28,22 +32,26 @@ export default function ScrollManagre({
   }, [section]);
 
   useFrame(() => {
+    if (!scroll.scroll) return;
+
     if (isAnimating.current) {
-      lastScroll.current = scroll.offset;
+      lastScroll.current = scroll.scroll.current;
       return;
     }
+    console.log(scroll.offset, scroll.pages, scroll, 'SCROLL');
+    l;
 
-    const currentSection = Math.floor(scroll.offset * scroll.pages);
-
-    if (scroll.offset > lastScroll.current && currentSection === 0) {
+    const currentSection = Math.floor(scroll.scroll.current * scroll.pages);
+    if (scroll.scroll.current > lastScroll.current && currentSection === 0) {
       setSection(1);
     }
     if (
-      scroll.offset < lastScroll.current &&
-      scroll.offset < 1 / (scroll.pages - 1)
+      scroll.scroll.current < lastScroll.current &&
+      scroll.scroll.current < 1 / (scroll.pages - 1)
     ) {
       setSection(0);
     }
+    lastScroll.current = scroll.scroll.current;
   });
 
   return null;
