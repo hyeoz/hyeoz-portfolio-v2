f48afe5fbb16c59b4cd23c98ddf9eb31e6eab802
hyeoz <jenny970203@gmail.com>
2024-03-12 00:51:10 +0900

    add: 축구공 애니메이션 추가

--- src/components/Football.tsx
+++ src/components/Football.tsx
@@ -1,34 +1,57 @@
-import { Environment, useGLTF } from '@react-three/drei';
-import { GroupProps } from '@react-three/fiber';
-import { Material, Object3D } from 'three';
+import { Environment, useAnimations, useGLTF } from '@react-three/drei';
+import { GroupProps, useFrame } from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import { Group, Material, Object3D, Object3DEventMap } from 'three';
 
 export default function Football(props: GroupProps) {
+  const group = useRef<Group<Object3DEventMap>>(null);
   const {
     nodes,
     materials,
+    animations,
   }: {
     nodes: { [name: string]: Object3D & { geometry?: any } };
     materials: { [name: string]: Material };
-  } = useGLTF('./models/football.glb');
+    animations: any;
+  } = useGLTF('./models/football-animation.glb');
+
+  const { actions, names, mixer } = useAnimations(animations, group);
+
+  useEffect(() => {
+    actions[names[0]]?.reset().play();
+  }, []);
+  console.log(actions, names);
   return (
     <>
       <Environment preset="forest" />
-      <group {...props} dispose={null} position={[0, -23, 0]} scale={2}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Icosphere001.geometry}
-          material={materials['White.001']}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Icosphere001_1.geometry}
-          material={materials['Black.001']}
-        />
+      <group
+        {...props}
+        ref={group}
+        dispose={null}
+        position={[0, -23, 0]}
+        scale={1.5}
+      >
+        <group name="Scene">
+          <group name="Icosphere">
+            <mesh
+              name="Icosphere001"
+              castShadow
+              receiveShadow
+              geometry={nodes.Icosphere001.geometry}
+              material={materials['White.001']}
+            />
+            <mesh
+              name="Icosphere001_1"
+              castShadow
+              receiveShadow
+              geometry={nodes.Icosphere001_1.geometry}
+              material={materials['Black.001']}
+            />
+          </group>
+        </group>
       </group>
     </>
   );
 }
 
-useGLTF.preload('./models/football.glb');
+useGLTF.preload('./models/football-animation.glb');
