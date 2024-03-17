e81222071bc8ea820e156787a3c64d0bcddab7f4
hyeoz <jenny970203@gmail.com>
2024-03-17 18:15:11 +0900

    add: fallback loading 화면 구현 중 (progress bar)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Suspense } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 
-import Scene from './Scene';
+// import Scene from './Scene';
+const Scene = lazy(() => import('./Scene'));
 import './styles/App.css';
+import { useLoad } from './store/load';
 
-/* TODO 
-Suspense 사용 방법 다시 확인
-Lazy Loading 사용
+/* NOTE 
+Suspense : 렌더링 하려는 자식 컴포넌트가 일시정지되면 fallback 화면을 띄워줌
+lazy : 처음 렌더링될 때까지 컴포넌트 코드를 지연시킴
 */
 
 function App() {
@@ -20,6 +22,21 @@ function App() {
 }
 
 function Loader() {
+  // TODO load 값으로 progress bar 만들기
+  const [loadingState, setLoadingState] = useState(0);
+  const load = useLoad;
+
+  useEffect(() => {
+    const unsubscribe = load.subscribe((state) => {
+      console.log(state.state);
+    });
+
+    return () => {
+      // 구독 해제
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div
       style={{
