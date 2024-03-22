9a38754e12f0bb30187495bbfdbd4316ce8ed7e4
hyeoz <jenny970203@gmail.com>
2024-03-23 00:00:13 +0900

    feat: 랜딩페이지 작업 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { useProgress } from '@react-three/drei';
 
@@ -12,11 +12,16 @@ lazy : 처음 렌더링될 때까지 컴포넌트 코드를 지연시킴
 */
 
 function App() {
+  const [isScrollPage, setIsScrollPage] = useState(false);
   return (
     <Suspense fallback={<Loader />}>
-      <Canvas id="canvas">
-        <Scene />
-      </Canvas>
+      {isScrollPage ? (
+        <Canvas id="canvas">
+          <Scene />
+        </Canvas>
+      ) : (
+        <Landing setState={setIsScrollPage} />
+      )}
     </Suspense>
   );
 }
@@ -30,6 +35,17 @@ function Loader() {
         <progress id="loading-progress" value={progress} max={100} />
       </div>
       <p className="progress-number">{progress.toFixed()}%</p>
+    </div>
+  );
+}
+
+function Landing({ setState }: { setState: any }) {
+  // TODO 사파리 초기 화면 따라한 랜딩페이지
+  // TODO 랜딩페이지에서 버튼 클릭 시 스트롤 섹션으로 넘어갈 수 있도록 설정
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <button onClick={() => setState(true)}>set 3D</button>
     </div>
   );
 }
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,7 +1,5 @@
 import { useState, lazy, useRef } from 'react';
 import { Environment, Html, Scroll, ScrollControls } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
-import { Camera, Group, Object3DEventMap, Vector3 } from 'three';
 
 const ScrollManager = lazy(() => import('./components/ScrollManager'));
 const HtmlContent = lazy(() => import('./components/HtmlContent'));
@@ -13,28 +11,12 @@ const Football = lazy(() => import('./components/Football'));
 
 function Scene() {
   const [section, setSection] = useState(0);
-  const modelScrollRef = useRef<Group<Object3DEventMap> & HTMLDivElement>(null);
-
-  const moveCamera = (
-    camera: Camera & {
-      manual?: boolean | undefined;
-    },
-    targetPosition: Vector3
-  ) => {
-    camera.position.lerp(targetPosition, 0.1);
-  };
-  useFrame(({ camera }) => {
-    const scrollPosition = new Vector3();
-    modelScrollRef.current?.getWorldPosition(scrollPosition);
-    // console.log(scrollPosition, '??? POSITION');
-    // moveCamera(camera, new Vector3(0, 0, -0.01));
-  });
 
   return (
     <>
       <ScrollControls pages={4} damping={0.1}>
         <ScrollManager section={section} setSection={setSection} />
-        <Scroll ref={modelScrollRef}>
+        <Scroll>
           <Environment preset="city" />
           <Laptop />
           <Airplane />
@@ -42,10 +24,6 @@ function Scene() {
           <Baseball />
           {/* <Particles size={7000} /> */}
         </Scroll>
-        {/* <Scroll
-        html
-        >
-      </Scroll> */}
       </ScrollControls>
       <Html>
         <HtmlContent />
