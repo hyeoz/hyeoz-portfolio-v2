04394673f2267643b36c4497e13f8c3f4e9d872c
hyeoz <jenny970203@gmail.com>
2024-03-24 19:10:25 +0900

    chore: 코드분리

---
+++ src/components/Landing.tsx
@@ -0,0 +1,7 @@
+function Landing({ setState }: { setState: any }) {
+  // TODO 사파리 초기 화면 따라한 랜딩페이지
+  // TODO 랜딩페이지에서 버튼 클릭 시 스트롤 섹션으로 넘어갈 수 있도록 설정
+  return <article className="landing-wrapper"></article>;
+}
+
+export default Landing;
---
+++ src/components/Loading.tsx
@@ -0,0 +1,15 @@
+import { useProgress } from '@react-three/drei';
+
+function Loading() {
+  const { progress } = useProgress();
+
+  return (
+    <article className="loading-wrapper">
+      <div className="progress-wrapper">
+        <progress id="loading-progress" value={progress} max={100} />
+      </div>
+      <p className="progress-number">{progress.toFixed()}%</p>
+    </article>
+  );
+}
+export default Loading;
