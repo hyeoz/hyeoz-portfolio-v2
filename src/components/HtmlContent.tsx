2eb73f3dd5a2a5f47d3d4c67190d01fafc4f71a1
hyeoz <jenny970203@gmail.com>
2024-04-03 15:56:42 +0900

    feat: swiper 컴포넌트 개발 중, 독도 works 컨텐츠 추가

--- src/components/HtmlContent.tsx
+++ src/components/HtmlContent.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import '../styles/content.css';
 import Modal from './Modal';
+import Swiper from './Swiper';
 
 /* TODO
   - HTML 찐 내용 채우기 (텍스트)
@@ -234,15 +235,53 @@ function Shakerrr() {
           <div className="stack-tag">ANTD</div>
         </div>
       </div>
-      <div>프로젝트 이미지가 슬라이더로 들어갑니다</div>
+      <div>
+        <Swiper />
+      </div>
     </div>
   );
 }
 function Dokdo() {
   return (
     <div className="works-modal-wrapper">
-      <div>프로젝트 설명이 들어갑니다</div>
-      <div>프로젝트 이미지가 슬라이더로 들어갑니다</div>
+      <div>
+        <ul>
+          <li>
+            독도체험관이 이전함에 따라 노후화된 웹페이지를 차세대 기술을
+            이용하여 새롭게 개발하였습니다.
+          </li>
+          <li>
+            관리자 페이지를 도맡아 개발하였고, 디자인 없이 핵심 기능만 구현
+            가능하도록 개발되었습니다.
+          </li>
+          <ul>
+            <li>
+              예약관리 기능을 개발하여 관리자가 관람 예약 현황을 확인하고 취소
+              가능하도록 하였습니다.
+            </li>
+            <li>
+              관람통계 관리 기능을 개발하여 기간 / 구분에 따라 관람객 통계를
+              확인하고 엑셀파일로 저장 가능하도록 하였습니다.
+            </li>
+            <li>
+              게시판 기능을 개발하여 유저페이지에 노출되는 공지사항, 교육자료,
+              갤러리 등을 관리할 수 있도록 하였습니다.
+            </li>
+          </ul>
+          <li>
+            <a>http://admindokdomuseum.nahf.kr/</a>
+          </li>
+        </ul>
+        <div className="stack-tag-wrapper">
+          <div className="stack-tag">TypeScript</div>
+          <div className="stack-tag">Next.js</div>
+          <div className="stack-tag">ANTD</div>
+          <div className="stack-tag">pm2</div>
+        </div>
+      </div>
+      <div>
+        <Swiper />
+      </div>
     </div>
   );
 }
@@ -250,7 +289,9 @@ function Wevent() {
   return (
     <div className="works-modal-wrapper">
       <div>프로젝트 설명이 들어갑니다</div>
-      <div>프로젝트 이미지가 슬라이더로 들어갑니다</div>
+      <div>
+        <Swiper />
+      </div>
     </div>
   );
 }
@@ -258,7 +299,9 @@ function Kovo() {
   return (
     <div className="works-modal-wrapper">
       <div>프로젝트 설명이 들어갑니다</div>
-      <div>프로젝트 이미지가 슬라이더로 들어갑니다</div>
+      <div>
+        <Swiper />
+      </div>
     </div>
   );
 }
