d9b8b0f98fd3d0cb62751019cfae94dc04865303
hyeoz <jenny970203@gmail.com>
2024-04-04 16:37:02 +0900

    feat: 절대경로 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
@@ -6,5 +7,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: [{ find: '@public', replacement: resolve(__dirname, 'public') }],
   },
 });
