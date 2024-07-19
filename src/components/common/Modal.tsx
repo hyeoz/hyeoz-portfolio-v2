a09291c0248051a99e9760fe804aeee4f4836c27
hyeoz <jenny970203@gmail.com>
2024-07-19 23:10:42 +0900

    add: X 아이콘 추가

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -19,7 +19,16 @@ function Modal({ open, title, content, onClose }: ModalPropsType) {
       <div className="modal-wrapper">
         <div className="modal-header">
           <h1>{title}</h1>
-          <h1 onClick={onClose}>X</h1>
+          <h1 onClick={onClose}>
+            <img
+              src="/svg/close_x.svg"
+              alt="modal-close-x"
+              style={{
+                width: '16px',
+                height: '16px',
+              }}
+            />
+          </h1>
         </div>
         <div className="modal-content">{content}</div>
       </div>
